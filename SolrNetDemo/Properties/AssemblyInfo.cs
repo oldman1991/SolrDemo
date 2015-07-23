9aef29533990b386de799fcfd206810ac847187b
lipeng1991 <619922692@qq.com>
2015-07-23 11:53:55 +0800

    哈哈哈

--- SolrNetDemo/Properties/AssemblyInfo.cs
+++ SolrNetDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SolrNet")]
+[assembly: AssemblyTitle("SolrNetDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SolrNet")]
+[assembly: AssemblyProduct("SolrNetDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("42dd3596-f09a-4d8d-af87-b8d45a581a9e")]
+[assembly: Guid("7ae57357-6e72-44dc-a7fa-b5a9d06c8890")]
 
 // 程序集的版本信息由下面四个值组成: 
 //
