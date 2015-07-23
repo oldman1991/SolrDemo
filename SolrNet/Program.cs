c226c5a217f8b109ed2aa63eee93449d8ee30d0e
lipeng1991 <619922692@qq.com>
2015-07-23 09:43:57 +0800

    新建

---
+++ SolrNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SolrNetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictsTr = new Dictionary<string, string>();
+            dictsTr.Add("message", "数据库");
+            DateTime start = DateTime.Now.AddDays(-3);
+            DateTime end = DateTime.Now.AddDays(-2);
+            int count;
+            var solr=new SolrNetOperate();
+                solr.GroupingSerach(dictsTr, 0, 10, start, end,out count);
+        }
+    }
+}
