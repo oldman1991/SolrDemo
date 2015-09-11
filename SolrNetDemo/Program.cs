7a68eb374efcc1eb137ff70c43425d7c767ae2af
619922692@qq.com <lipeng1991>
2015-09-11 10:10:25 +0800

    修改一下下

--- SolrNetDemo/Program.cs
+++ SolrNetDemo/Program.cs
@@ -12,10 +12,12 @@ namespace SolrNetDemo
         {
             Dictionary<string,string>paraDict=new Dictionary<string, string>();
             paraDict.Add("message","数据库11");
+
             DateTime start = DateTime.Now.AddDays(-3);
             DateTime end = DateTime.Now.AddDays(-1);
             int count;
           var res=  SolrNetOperate.GroupingSerach(paraDict, 0, 10, start, end, out count);
+            Console.ReadKey();
         }
     }
 }
